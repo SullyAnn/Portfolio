088e3303624938386babf9e5ed3caa1208f93d5d
SullyAnn <sori.sully@gmail.com>
2022-12-14 10:57:12 +0100

    favicon and small details

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,5 @@
-"use strict";
-
-
 /*---------------------!!  DETAIL JS   !!---------------------*/
 
-
-"use strict";
 
 
 /*--------------------- TRANSITIONS A  ---------------------*/
@@ -12,59 +7,58 @@
 let validation = false;
 
 window.onload = () => {
-  const transitionA = document.querySelector(".transition1");
-  const Buttons = document.querySelectorAll(".buttonTrans1");
+	const transitionA = document.querySelector(".transition1");
+	const Buttons = document.querySelectorAll(".buttonTrans1");
 
-  if (validation==false){
-    setTimeout(() => {
-      transitionA.classList.remove("is-active");
-    }, 500);
-     
-  }
+	if (validation == false) {
+		setTimeout(() => {
+			transitionA.classList.remove("is-active");
+		}, 500);
 
-  Buttons.forEach(button => {
-    button.addEventListener('click', function(event) {
-      event.preventDefault();
-      let target = event.target.href;
-      transitionA.classList.add("is-active");
-  
-      validation = true;
-  
-      setTimeout(() => {
-        window.location.href = target;
-      }, 500);
-  
-    })
-  })
-}
+	}
+
+	Buttons.forEach(button => {
+		button.addEventListener('click', function(event) {
+			event.preventDefault();
+			let target = event.target.href;
+			transitionA.classList.add("is-active");
+
+			validation = true;
+
+			setTimeout(() => {
+				window.location.href = target;
+			}, 500);
+
+		});
+	});
+};
 
 /*--------------------- MENU APPEARANCE ---------------------*/
 
 const menu = document.getElementById("menuOpen");
 const menuListener = document.querySelector("#navBar h2 a");
-const menuClose = document.querySelector("#close a"); 
+const menuClose = document.querySelector("#close a");
 const main = document.querySelector("#snaphere");
-const body = document.querySelector("body");
 
 
-menuListener.addEventListener('click', function(event){
-    event.preventDefault();
-    if(menu.classList.contains("hiddenOut")){
-        menu.classList.replace("hiddenOut", "hidden");
-    }  
-    menu.classList.replace("hidden", "visible");
-    main.classList.add("filterMode");
-    //body.classList.add("noOverflow");
+menuListener.addEventListener('click', function(event) {
+	event.preventDefault();
+	if (menu.classList.contains("hiddenOut")) {
+		menu.classList.replace("hiddenOut", "hidden");
+	}
+	menu.classList.replace("hidden", "visible");
+	main.classList.add("filterMode");
+	//body.classList.add("noOverflow");
 
-  
-})
 
-menuClose.addEventListener('click', function(event){
-    event.preventDefault();
-    menu.classList.replace("visible", "hiddenOut");
-    main.classList.remove("filterMode");
-    //body.classList.remove("noOverflow");
-})
+});
+
+menuClose.addEventListener('click', function(event) {
+	event.preventDefault();
+	menu.classList.replace("visible", "hiddenOut");
+	main.classList.remove("filterMode");
+	//body.classList.remove("noOverflow");
+});
 
 /*--------------------- MENU ANIMATION SCROLL  ---------------------*/
 
@@ -81,32 +75,31 @@ function scrollFunction() {
   }
 */
 
-  var prevScrollpos= window.pageYOffset;
-    window.onscroll=function(){
-    var currentScrollpos=window.pageYOffset;
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	var currentScrollpos = window.pageYOffset;
 
-    if(prevScrollpos> currentScrollpos){
-        menuBar.style.top="0";
-    }
-    else{
-        menuBar.style.top="-100px"
-    }
-    prevScrollpos=currentScrollpos
-    }
+	if (prevScrollpos > currentScrollpos) {
+		menuBar.style.top = "0";
+	} else {
+		menuBar.style.top = "-100px";
+	}
+	prevScrollpos = currentScrollpos;
+};
 
 /*--------------------- SLIDESHOW   ---------------------*/
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 
 });
 
